50bdac46b572ca9484f5eb3ef55b46ed76bf6f0c
aish8la <64914670+aish8la@users.noreply.github.com>
2024-04-28 03:16:25 +0500

    add eventListener, placeholder function for hover

--- scripts.js
+++ scripts.js
@@ -5,20 +5,28 @@ let numberOfGridsPerAxis = 10;
 let sizeOfSketchBox = sketchBox.clientWidth;
 let totalNumberOfGrids = numberOfGridsPerAxis**2;
 let gridSize = `${Math.round(sizeOfSketchBox / numberOfGridsPerAxis)}px`;
+let brushMode = 'black';
 
 for (i = 1; i <= totalNumberOfGrids; i++) {
     const grids = document.createElement('div');
-    const className = `grid-${i}`;
-    grids.classList.add('grids', className);
+    const idName = `grid-${i}`;
+    grids.classList.add('grids');
+    grids.setAttribute('id', idName);
     grids.setAttribute('style', `height: ${gridSize}; width: ${gridSize}`)
     sketchBox.appendChild(grids);
 }
 
+sketchBox.addEventListener('mouseover', (event)=> {
+    let hoverGrid = event.target;
+    hoverAction(hoverGrid);
+
+});
+
+function hoverAction(element) {
+    console.log(element.id);
+}
 
 
-
-//use this variable to display the needed value in the title of the page
-debugValue = extraPixel;
 
 
 
